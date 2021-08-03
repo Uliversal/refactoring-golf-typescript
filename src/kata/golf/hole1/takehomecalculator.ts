22f044ddb2ba8260f6bba9ca780ce90cdd7f40e6
Javi <javier.lopez@cluno.com>
2021-08-03 11:28:08 +0200

    mob next [ci-skip] [ci skip] [skip ci]

--- src/kata/golf/hole1/takehomecalculator.ts
+++ src/kata/golf/hole1/takehomecalculator.ts
@@ -1,21 +1,5 @@
 import { Incalculable } from "./incalculable";
-
-export class Money {
-  public value: number;
-  public currency: string;
-
-  constructor(value: number, currency: string) {
-    this.value = value;
-    this.currency = currency;
-  }
-
-  plus(other: Money): Money {
-    if (other.currency !== this.currency) {
-      throw new Incalculable();
-    }
-    return new Money(this.value + other.value, other.currency);
-  }
-}
+import { money, Money } from "./money";
 
 export class Takehomecalculator {
   private percent: number;
@@ -24,20 +8,20 @@ export class Takehomecalculator {
     this.percent = percent;
   }
 
-  netAmount(value: Money, ...rest: Money[]): Money {
+  netAmount(first: Money, ...rest: Money[]): Money {
     const monies: Array<Money> = Array.from(rest);
-    let total: Money = value;
+    let total: Money = first;
 
     for (const next of monies) {
       total = total.plus(next);
     }
 
     const amount: number = total.value * (this.percent / 100.0);
-    const tax: Money = new Money(Math.trunc(amount), value.currency);
+    const tax: Money = money(Math.trunc(amount), first.currency);
 
     if (total.currency !== tax.currency) {
       throw new Incalculable();
     }
-    return new Money(total.value - tax.value, value.currency);
+    return total.minus(tax);
   }
 }
